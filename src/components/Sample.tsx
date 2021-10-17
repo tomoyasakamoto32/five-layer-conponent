45137dcaf3f47294305e03eeb2aafbc4109877ed
tomoyasakamoto32 <oregairu0302@gmail.com>
2021-10-17 18:48:24 +0900

    conflict

--- src/components/Sample.tsx
+++ src/components/Sample.tsx
@@ -4,7 +4,7 @@ import Button from '@mui/material/Button'
 /**
  * @private
  */
-export const PlainSample = (): JSX.Element => <Button variant="contained">Hello World</Button>
+export const PlainSample = (): JSX.Element => <Button variant="contained">Hello World!!!</Button>
 
 const Sample = (): JSX.Element => {
   return <PlainSample />
